3626832a8809841e6288e1c1e4fa42c327cadec3
emi ota <ametio.145@gmail.com>
2021-08-23 08:11:22 +0000

    お気に入りした時に一覧画面にとばないようにした

--- app/controllers/favorite_animals_controller.rb
+++ app/controllers/favorite_animals_controller.rb
@@ -1,11 +1,11 @@
 class FavoriteAnimalsController < ApplicationController
   def create
     favorite_animal = current_user.favorite_animals.create(animal_id: params[:animal_id])
-    redirect_to animals_path, notice: "#{favorite_animal.animal.name}をお気に入り登録しました"
+    redirect_to animal_path(favorite_animal.animal.id), notice: "#{favorite_animal.animal.name}をお気に入り登録しました"
   end
 
   def destroy
     favorite_animal = current_user.favorite_animals.find_by(id: params[:id]).destroy
-    redirect_to animals_path, notice: "#{favorite_animal.animal.name}をお気に入り解除しました"
+    redirect_to animal_path(favorite_animal.animal.id), notice: "#{favorite_animal.animal.name}をお気に入り解除しました"
   end
 end
