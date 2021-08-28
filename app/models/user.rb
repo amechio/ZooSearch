7e6212ea54221460cc0f5ba348fe72b827723152
emi ota <ametio.145@gmail.com>
2021-08-28 18:39:22 +0000

    ゲストユーザのメールアドレスを変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,14 +13,14 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
   def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |user|
+    find_or_create_by!(email: 'guest@email.com') do |user|
       user.name = "ゲスト"
       user.password = SecureRandom.urlsafe_base64
     end
   end
 
   def self.guest_admin
-    find_or_create_by!(email: 'admin@example.com') do |user|
+    find_or_create_by!(email: 'admin@email.com') do |user|
       user.name = "ゲスト管理者"
       user.password = SecureRandom.urlsafe_base64
       user.admin = true
