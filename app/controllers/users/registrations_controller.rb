e6ee218da75c6518c804546034c4f630e45b42e8
emi ota <ametio.145@gmail.com>
2021-09-07 14:38:10 +0000

    マイページ編集機能を変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -19,6 +19,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
+  protected
+
   def configure_account_update_params
    devise_parameter_sanitizer.permit(:account_update, keys: [
      :name,
