88ddeca59556571bb7e825d5ad4252c0db82c80e
emi ota <ametio.145@gmail.com>
2021-09-15 14:03:23 +0000

    マイページ編集機能を変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,91 +1,62 @@
+# frozen_string_literal: true
+
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :ensure_normal_user, only: [:destroy, :destroy]
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
 
-  def ensure_normal_user
-    if resource.email == 'guest@example.com' || resource.email == 'admin@example.com'
-      redirect_to root_path, alert: 'ゲストユーザーの更新・削除はできません！'
-    end
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  # def update
-  #   @user.assign_attributes(account_update_params)
-  #   if @user.save
-  #     bypass_sign_in(@user)
-	#     redirect_to user_path(id: @user.id), notice: 'プロフィールを更新しました'
-  #     # redirect_to root_path, notice: "プロフィールを編集しました！もう一度ログインしてください"
-  #   else
-  #     render "edit"
-  #   end
+  # GET /resource/sign_up
+  # def new
+  #   super
   # end
 
-  def update
-    if by_admin_user?(params)
-      self.resource = resource_class.to_adapter.get!(params[:id])
-    else
-      self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-    end
+  # POST /resource
+  # def create
+  #   super
+  # end
 
-    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
 
-    if by_admin_user?(params)
-      resource_updated = update_resource_without_password(resource, account_update_params)
-    else
-      resource_updated = update_resource(resource, account_update_params)
-    end
+  # PUT /resource
+  # def update
+  #   super
+  # end
 
-    yield resource if block_given?
-    if resource_updated
-      if is_flashing_format?
-        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
-          :update_needs_confirmation : :updated
-        set_flash_message :notice, flash_key
-      end
-      if !by_admin_user?(params)
-        bypass_sign_in resource, scope: resource_name
-      end
-      respond_with resource, location: after_update_path_for(resource)
-    else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
-    end
-  end
+  # DELETE /resource
+  # def destroy
+  #   super
+  # end
 
-  protected
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
 
-  def update_params
-   params.require(:user).permit(:update, keys: [
-     :name,
-     :email,
-     # :password,
-     # :password_confirmation,
-     :live_prefecture,
-     :favorite_prefecture,
-     :content,
-     :icon,
-     :owner,
-     :admin
-     ])
-  end
+  # protected
 
-  def update_resource(resource, params)
-    if params[:password].present? && params[:password_confirmation].present?
-      resource.update_attributes(params)
-    else
-      resource.update_without_password(params)
-    end
-  end
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # end
 
-  def after_update_path_for(resource)
-    user_profile_path(current_user)
-  end
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
 
-  def by_admin_user?(params)
-    params[:id].present? && current_user_is_admin?
-  end
+  # The path used after sign up.
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
 
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
 end
