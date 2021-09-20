e9f54484883f210d8fcc4724a65fdacaf2287f06
emi ota <ametio.145@gmail.com>
2021-09-20 14:33:31 +0000

    マイページ編集機能を変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  before_action :ensure_normal_user, only: [:destroy, :destroy]
+  before_action :ensure_normal_user, only: [:update, :destroy]
 
   def ensure_normal_user
     if resource.email == 'guest@example.com' || resource.email == 'admin@example.com'
@@ -113,6 +113,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def by_admin_user?(params)
     params[:id].present? && current_user_is_admin?
+  end
+
+  def after_update_path_for(resource)
+    user_path(@user.id)
   end
 
   # If you have extra params to permit, append them to the sanitizer.
