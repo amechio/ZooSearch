04d2bf2ec0085a9165530dcc8be56373f5f0e598
emi ota <ametio.145@gmail.com>
2021-10-02 13:43:02 +0000

    マイページ編集機能を変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -23,66 +23,66 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # GET /resource/edit
   # def edit
-  def profile_edit
-    # super
-    @user = User.find(params[:id])
-  end
-
-  # PUT /resource
-  # def update
-  def profile_update
-    # super
-
-    @user.assign_attributes(account_update_params)
-    # if @user.save
-    #   bypass_sign_in(@user)
-	  #   # redirect_to user_path(id: @user.id), notice: 'プロフィールを更新しました'
-    #   redirect_to root_path, notice: "プロフィールを編集しました！もう一度ログインしてください"
-    # else
-    #   render "edit"
-    # end
-
-    original_user = current_user
-
-    if by_admin_user?(params)
-      self.resource = resource_class.to_adapter.get!(params[:id])
-    else
-      self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-    end
-
-    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
-
-    if by_admin_user?(params)
-      resource_updated = update_resource_without_password(resource, account_update_params)
-    else
-      resource_updated = update_resource(resource, account_update_params)
-    end
-
-    yield resource if block_given?
-
-    if resource_updated
-      if is_flashing_format?
-        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
-          :update_needs_confirmation : :updated
-        set_flash_message :notice, flash_key
-      end
-      if !by_admin_user?(params)
-        # bypass_sign_in resource, scope: resource_name
-        sign_in(resource_name, original_user)
-        respond_with resource, location: after_sign_up_path_for(resource)
-      end
-      respond_with resource, location: after_update_path_for(resource)
-    else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
-    end
-  end
-
-  # DELETE /resource
-  def destroy
-    super
-  end
+  # def profile_edit
+  #   # super
+  #   @user = User.find(params[:id])
+  # end
+  #
+  # # PUT /resource
+  # # def update
+  # def profile_update
+  #   # super
+  #
+  #   @user.assign_attributes(account_update_params)
+  #   # if @user.save
+  #   #   bypass_sign_in(@user)
+	#   #   # redirect_to user_path(id: @user.id), notice: 'プロフィールを更新しました'
+  #   #   redirect_to root_path, notice: "プロフィールを編集しました！もう一度ログインしてください"
+  #   # else
+  #   #   render "edit"
+  #   # end
+  #
+  #   original_user = current_user
+  #
+  #   if by_admin_user?(params)
+  #     self.resource = resource_class.to_adapter.get!(params[:id])
+  #   else
+  #     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+  #   end
+  #
+  #   prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+  #
+  #   if by_admin_user?(params)
+  #     resource_updated = update_resource_without_password(resource, account_update_params)
+  #   else
+  #     resource_updated = update_resource(resource, account_update_params)
+  #   end
+  #
+  #   yield resource if block_given?
+  #
+  #   if resource_updated
+  #     if is_flashing_format?
+  #       flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+  #         :update_needs_confirmation : :updated
+  #       set_flash_message :notice, flash_key
+  #     end
+  #     if !by_admin_user?(params)
+  #       # bypass_sign_in resource, scope: resource_name
+  #       sign_in(resource_name, original_user)
+  #       respond_with resource, location: after_sign_up_path_for(resource)
+  #     end
+  #     respond_with resource, location: after_update_path_for(resource)
+  #   else
+  #     clean_up_passwords resource
+  #     set_minimum_password_length
+  #     respond_with resource
+  #   end
+  # end
+  #
+  # # DELETE /resource
+  # def destroy
+  #   super
+  # end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
